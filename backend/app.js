34c28e76a22f4df0b3ac4a4491f4bb4ab909476e
manolo0o <147361804+manolo0o@users.noreply.github.com>
2025-02-18 17:39:08 -0500

    feat: :white_check_mark: adding routes to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,26 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const Connect = require('./conf/conection.js');
 const cors = require('cors');
+
 const db = new Connect();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+//routes imports
+const clienteRoutes = require('./routes/cliente_route.js');
+const articuloRoutes = require('./routes/articulo_route.js');
+const facturaRoutes = require('./routes/factura_route.js');
+const kardexRoutes = require('./routes/kardex_route.js');
+
 app.use(cors());
 app.use(express.json());
+
+// api routes
+app.use('/clients', clienteRoutes);
+app.use('/article', articuloRoutes);
+app.use('/factura', facturaRoutes);
+app.use('/kardex', kardexRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
