c9cf0f7c17ff58c89ec608ec7e1cd096f73a4f42
manolo0o <147361804+manolo0o@users.noreply.github.com>
2025-02-23 22:50:00 -0500

    feat: :zap: Updating Kardex model

--- backend/models/kardex_model.js
+++ backend/models/kardex_model.js
@@ -1,16 +1,27 @@
 const mongoose = require('mongoose');
 
 const facturaKardexSchema = new mongoose.Schema({
-    FK_CodProd: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Articulo',
+    FK_Fecha_Ingreso: {
+        type: Date,
+        required:true
+    },
+    FK_Nom: {
+        type: String,
         required: true
     },
-    FK_CantProd: {
+    FK_Lab: {
+        type: String,
+        required: true
+    },
+    FK_Saldo: {
         type: Number,
         required: true
     },
     FK_Costo: {
+        type: Number,
+        required: true
+    },
+    FK_PV: {
         type: Number,
         required: true
     }
